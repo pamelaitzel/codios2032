242ee9bbf7d3e63300dcac692af96beabe62bdb7
pame <135767172+pamelaitzel@users.noreply.github.com>
2024-03-04 02:46:41 -0600

    Update wen.c

--- wen.c
+++ wen.c
@@ -1,10 +1,13 @@
 
 
 int main() {
-    for (int i = 1; i < 1000; i++) {
-        if (i % 2 == 0 && i % 7 == 0) {
+    int i = 1;
+    while (i < 100) {
+        if (i % 7 != 0) {
             printf("%d\n", i);
         }
+        i++;
     }
     return 0;
 }
+
